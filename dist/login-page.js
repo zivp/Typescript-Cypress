611f2d6975e2a2e05d16502f361851b138a17188
zivp <zivp1010@gmail.com>
2021-10-09 17:59:29 +0300

    update pacage.json

--- dist/login-page.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var LoginPage = /** @class */ (function () {
-    function LoginPage() {
-    }
-    return LoginPage;
-}());
-exports.default = LoginPage;
